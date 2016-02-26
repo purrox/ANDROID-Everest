11dcf584b0e22016436cfca6043d4d06c8108660
Carlos Andres Salas <carlos.salas@avantica.net>
2016-02-26 00:12:25 -0600

    no message

---
+++ App1/app/src/main/java/com/example/csalas/app1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.csalas.app1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView greetingView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_bye){
+            greetingView = (TextView) findViewById(R.id.greetings);
+            greetingView.setText(R.string.bye);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
